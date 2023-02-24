f04cf12a7400e3da0ee30cfeb17e7c7699b478e1
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-02-24 10:40:41 +0300

    no message

---
+++ app/src/main/java/com/example/gelismiskomutlarunite5/Dortgen.java
@@ -0,0 +1,21 @@
+package com.example.gelismiskomutlarunite5;
+
+public class Dortgen {
+    public Integer kisaKenar;
+    public Integer uzunKenar;
+
+    public Integer alaniBul(){
+    Integer alan;
+        alan = kisaKenar * uzunKenar;
+        return alan;
+    }
+
+    public Dortgen(Integer kisaKenar) {
+        this.kisaKenar = kisaKenar;
+    }
+
+    public Dortgen(Integer kisaKenar, Integer uzunKenar) {
+        this.kisaKenar = kisaKenar;
+        this.uzunKenar = uzunKenar;
+    }
+}
--- app/src/main/java/com/example/gelismiskomutlarunite5/Uyg6Activity.java
+++ app/src/main/java/com/example/gelismiskomutlarunite5/Uyg6Activity.java
@@ -3,6 +3,9 @@ package com.example.gelismiskomutlarunite5;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +15,38 @@ public class Uyg6Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uyg6_activity);
-    }
+        EditText editTextKisaKenar = findViewById(R.id.editTextTextPersonName);
+        EditText editTextUzunKenar = findViewById(R.id.editTextTextPersonName2);
+        Button buttonKareAlani = findViewById(R.id.btnkare);
+        Button buttonDikdortgenAlani = findViewById(R.id.btndikdörtgenalan);
+        TextView textViewAlan = findViewById(R.id.textView12);
 
+        buttonKareAlani.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Integer kenar;
+                Integer alan;
+                kenar = Integer.parseInt(editTextKisaKenar.getText().toString());
+                Dortgen kare = new Dortgen(kenar);
+                alan = kare.alaniBul();
+                textViewAlan.setText(alan.toString());
+            }
+        });
+        buttonDikdortgenAlani.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View view) {
+            Integer kisaKenar;
+            Integer uzunKenar;
+            Integer alan;
+            kisaKenar = Integer.parseInt(editTextKisaKenar.getText().toString());
+            uzunKenar = Integer.parseInt(editTextUzunKenar.getText().toString());
+            Dortgen dikdortgen = new Dortgen(kisaKenar,uzunKenar);
+            textViewAlan.setText(dikdortgen.toString());
+        }
+    });
+
+    }
     public void btnBack(View view) {
         Intent uyg = new Intent(Uyg6Activity.this, MainActivity.class);
         startActivity(uyg);
+
     }
 }
